40b811c7955f4bf189a55ab273f9db18e0359129
Jarrod Hanzlik <>
2019-01-04 22:03:13 -0600

    Updated POM. Jersey/Grizzly running in executable jar.

--- src/main/java/com/hanzlikstudio/mellow/Main.java
+++ src/main/java/com/hanzlikstudio/mellow/Main.java
@@ -1,3 +1,15 @@
+/*
+Copyright 2016 Janus Friis Nielsen
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 
 package com.hanzlikstudio.mellow;
 
@@ -7,39 +19,62 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Main class.
+ * This is the example entry point, where Jersey application for the example
+ * gets populated and published using the Grizzly 2 HTTP container.
  *
+ * @author Janus Friis Nielsen (jfn at fagidiot.dk)
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+    private static final URI BASE_URI = URI.create("http://localhost:8080/base/");
+    /**
+     * "Hello World" root resource path.
+     */
+    public static final String ROOT_PATH = "helloworld";
 
     /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
+     * Main application entry point.
+     *
+     * @param args application arguments.
      */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.example package
-        final ResourceConfig rc = new ResourceConfig().packages("com.hanzlikstudio.mellow");
+    public static void main(String[] args) {
+        try {
+            System.out.println("\"Hello World\" Jersey Example App");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, create(), false);
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    server.shutdownNow();
+                }
+            }));
+            server.start();
+
+            System.out.println(
+                    String.format("Application started.%n"
+                                    + "Try out %s%s%n"
+                                    + "Stop the application using CTRL+C",
+                            BASE_URI, ROOT_PATH));
+
+            Thread.currentThread().join();
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     /**
-     * Main method.
-     * @param args
-     * @throws IOException
+     * Create example application resource configuration.
+     *
+     * @return initialized resource configuration of the example application.
      */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+    public static ResourceConfig create() {
+        final ResourceConfig resourceConfig = new ResourceConfig(MyResource.class);
+
+        return resourceConfig;
     }
 }
--- src/main/java/com/hanzlikstudio/mellow/MyResource.java
+++ src/main/java/com/hanzlikstudio/mellow/MyResource.java
@@ -1,4 +1,15 @@
-
+/*
+Copyright 2016 Janus Friis Nielsen
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package com.hanzlikstudio.mellow;
 
 import javax.ws.rs.GET;
@@ -6,19 +17,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-// The Java class will be hosted at the URI path "/myresource"
-@Path("/myresource")
+/**
+ * @author Janus Friis Nielsen (jfn at fagidiot.dk)
+ */
+@Path("helloworld")
 public class MyResource {
+    public static final String CLICHED_MESSAGE = "Hello World!";
 
-     /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET    
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHello() {
+        return CLICHED_MESSAGE;
     }
+
 }
